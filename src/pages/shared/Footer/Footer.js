e0b5c5f74a4e22b64c57add5c8b909e98e8b66ab
mirzanahid <mirzasaikat3@gmail.com>
2022-11-07 23:26:56 +0600

    footer added

--- src/pages/shared/Footer/Footer.js
+++ src/pages/shared/Footer/Footer.js
@@ -1,20 +1,38 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Col, Container } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-
+import { FaFacebookF, FaInstagram, FaTwitter, FaLinkedin, FaWhatsapp } from "react-icons/fa";
+import './Footer.css'
 const Footer = () => {
     return (
+        <div className='footer'>
             <Container>
-            <div className="foot-logo">
-                <h1>logooooo</h1>
-            </div>
-            <div className="social">
-                <ul>
-                    <li><Link></Link></li>
-                </ul>
-            </div>
+                <div className="foot-logo">
+                    <h1>logooooo</h1>
+                </div>
+                <div className="links">
+                    <Link className='heading-2'>Home</Link>
+                    <Link className='heading-2'>Home</Link>
+                    <Link className='heading-2'>Home</Link>
+                    <Link className='heading-2'>Home</Link>
+                    <Link className='heading-2'>Home</Link>
 
-            </Container>
+
+                </div>
+                <div className="social">
+                    <ul>
+                        <li><Link><FaFacebookF /></Link></li>
+                        <li><Link><FaInstagram /></Link></li>
+                        <li><Link><FaTwitter /></Link></li>
+                        <li><Link><FaLinkedin /></Link></li>
+                        <li><Link><FaWhatsapp /></Link></li>
+                    </ul>
+                </div>
+                <div className="footer-bottom">
+                    <p>© Copyright 2022 | LoveLove | All right reserved.</p>
+                </div>
+            </Container >
+        </div>
     );
 };
 
